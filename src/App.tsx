e9d3e3502bcf9fee1611660ed07117259a866d31
Nazariy Tivirikin <n.tivirikin@gmail.com>
2024-03-25 17:03:50 -0400

    Add basic home page design and buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+import smallLogo from '/small_logo.png'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+  // const [view, setView] = useState('home')
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <img src={smallLogo} className="small-logo" alt="FishLogger small logo" />
+      <h1>FishLogger</h1>
+      
+      {/* This card will change on login or register to display different fields  */}
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button>
+          Login
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+        <button className="register-button"> 
+          Register 
+        </button>
+        <p> 
+          Welcome to FishLogger!
+          Please login with an existing account or register a new account.
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Lorem ipsum.
       </p>
     </>
   )
