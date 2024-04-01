ea4abbc4439df8412d1181f08aeae635602e8fc7
Nazariy Tivirikin <n.tivirikin@gmail.com>
2024-04-01 12:19:33 -0400

    Cleanup catch logging page and styles

--- src/components/Logging.tsx
+++ src/components/Logging.tsx
@@ -1,3 +1,7 @@
+import smallLogo from '/small_logo.png'
+
+import './Logging.css'
+
 interface LoggingProps {
     onBackClick: () => void;
 }
@@ -7,24 +11,41 @@ const Logging : React.FC<LoggingProps> = ( {onBackClick} ) => {
         <>
             <p>Please fill out the information below to log your catch!</p>
 
-            <div className="flex-container">
+            <div className="side-flex input-back">
+                <div className="flex-container">
 
-                {/* Common Name (Latin Name) */}
-                <span>Fish Species:</span>
-                <input /> {/* Typing brings down list */}
+                    <span>Date:</span>
+                    <input />
 
-                <span>Date:</span>
-                <input />
+                    {/* Common Name (Latin Name) */}
+                    <span>Fish Species:</span>
+                    <input /> {/* Typing brings down list */}
 
-                <span>Location:</span>
-                <input />
+                    <span>Location:</span>
+                    <input />
+                    
+                    {/* Optional */}
+                    <span>Length:</span>
+                    <input />
 
-                <span>Length:</span>
-                <input />
+                    {/* Optional */}
+                    <span>Weight:</span>
+                    <input />
 
-                <span>Weight:</span>
-                <input />
+                    {/* Optional */}
+                    <span>Weather:</span>
+                    <input />
 
+                    {/* Optional */}
+                    <span>Fishing Technique:</span>
+                    <input /> {/* Select from options:*/}
+
+                    {/* Optional */}
+                    <span>Lure/Bait:</span>
+                    <input /> {/* Select from options:*/}
+
+                </div>
+                <img src={smallLogo} className="fish-species" alt="Fish species photograph"/>
             </div>
 
             <div className="button-box">
